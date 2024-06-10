9cccb2f910c0362403ff900ae48b5d8b9ec8ed5c
johan253 <johannjo2000@gmail.com>
2024-06-10 15:47:32 -0700

    Fixed function exports

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,8 @@
-const admin = require
+const admin = require("firebase-admin");
 admin.initializeApp();
 
 // Import the funtion from the specific file
-const addMessage = require("./api/addMessage");
+const {addMessage} = require("./api/addMessage");
 
 // Export the function for deployment
 exports.addMessage = addMessage;
